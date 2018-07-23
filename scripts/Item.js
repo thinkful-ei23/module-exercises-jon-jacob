f17687fcc7e0bb3cd7c8454b0f5cea6891f0fc35
jonsung89 <jonsung89@gmail.com>
2018-07-23 11:01:04 -0500

    completed item factory and validator

--- scripts/Item.js
+++ scripts/Item.js
@@ -3,7 +3,22 @@
 
 const Item = (function () {
 
+  function validateName(name) {
+    if (!name || name === '') {    // if name doesn't exist
+      throw new TypeError('Name does not exist');
+    }
+  }  
+  function create(name) {
+    return {
+      id: cuid(),
+      name: name,
+      checked: false
+    };
+  }
+
   return {  
-     
+    validateName: validateName,
+    create: create
   };
 }());
+
--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,7 @@
 
 // eslint-disable-next-line no-unused-vars
 
-
-
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
 });
-
